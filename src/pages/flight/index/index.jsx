a288e15a751c61ab9114c4b1b1840914553735b8
luminous <910667665@qq.com>
2023-10-05 22:13:50 +0800

    feat:添加首页广告轮播图

--- src/pages/flight/index/index.jsx
+++ src/pages/flight/index/index.jsx
@@ -1,8 +1,9 @@
-import { View, SwiperItem } from "@tarojs/components";
+import { View, SwiperItem, Swiper, Image } from "@tarojs/components";
 import "./index.scss";
 
 // 组件
 import TabContent from "../../../components/Tab";
+import NoExploit from "../../../components/NoExploit";
 
 // 默认值
 const INIT_TABS = [
@@ -18,6 +19,12 @@ const INIT_TABS = [
     label: "往返",
     id: 2,
   },
+];
+
+// 图片mock
+const MOCK_IMG = [
+  `https://th.bing.com/th?id=OIP.Chh6O-1fhahYH0uBytAD9gHaE7&w=306&h=204&c=8&rs=1&qlt=90&o=6&dpr=2&pid=3.1&rm=2`,
+  `https://th.bing.com/th?id=OIP.rNIcMY3-JcKsSLcZztIhPAHaE7&w=306&h=204&c=8&rs=1&qlt=90&o=6&dpr=2&pid=3.1&rm=2`,
 ];
 
 export default function FlightContent() {
@@ -36,11 +43,23 @@ export default function FlightContent() {
           {/* 选项卡内容-插槽 */}
           {/* 测试app.scss 全局样式*/
           /* <SwiperItem className="color-red">111</SwiperItem> */}
-          <SwiperItem>111</SwiperItem>
-          <SwiperItem>22</SwiperItem>
-          <SwiperItem>33</SwiperItem>
+          <SwiperItem></SwiperItem>
+          <SwiperItem>
+            <NoExploit className="no-data"></NoExploit>
+          </SwiperItem>
+          <SwiperItem>
+            <NoExploit className="no-data"></NoExploit>
+          </SwiperItem>
         </TabContent>
       </View>
+      {/* 图片轮播 */}
+      <Swiper className="advs-banner-bd" autoplay>
+        {MOCK_IMG.map((img) => (
+          <SwiperItem key={img} className="item">
+            <Image className="img" src={img}></Image>
+          </SwiperItem>
+        ))}
+      </Swiper>
     </View>
   );
 }
